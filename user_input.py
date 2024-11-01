7212ee14326d16592ef8c625c691ecdd3c780e35
Nickyamari <kogeinicky@gmail.com>
2024-11-01 13:53:11 +0300

    1st code done

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+print("Hello {Nicole}, you are {19} years old and live in {Nairobi}.")
